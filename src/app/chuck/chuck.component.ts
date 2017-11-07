66acb9ad8bc48050eb9629df9ea865592e706ee6
Gideon Spierings <gspierings@vaa.com>
2017-11-07 21:03:29 +0100

    Added some comments to the plumbing since it is becoming somewhat complex

--- src/app/chuck/chuck.component.ts
+++ src/app/chuck/chuck.component.ts
@@ -46,21 +46,28 @@ export class ChuckComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    // Add all jokes to jokeService on api refresh
     this.apiService.jokes$
       .mergeMap(jokes => Observable.from(jokes))
       .subscribe(joke => this.jokeService.add(joke));
 
+    // Grab only the un-favorited jokes
     this.jokeService.jokes$
       .map(jokes => jokes.filter(joke => !joke.favorite))
       .subscribe(this.jokes$);
 
+    // Grab only the favorite jokes
     this.jokeService.jokes$
       .map(jokes => jokes.filter(joke => joke.favorite))
       .subscribe(this.favorites$);
 
+    // Persist favorites every update
     this.favorites$
       .subscribe(favs => this.storageService.storeFavorites(favs));
 
+    // TODO: Changedetection is not working for FavoritesComponent, though the favorites$ observable does updates correctly
+    // Load persisted favorites on init
     Observable
       .from(this.storageService.getFavorites())
       .subscribe(joke => this.jokeService.add(joke));
